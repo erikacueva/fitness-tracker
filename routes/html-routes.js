ef732f760df6988f6879ef2e479e56cf36e4fcbc
erika cueva <ecueva88@gmail.com>
2021-02-09 16:28:07 -0800

    created html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,1 +1,21 @@
-// looks like the notetaker html routes except you use router not app
+
+var path = require("path");
+
+module.exports = function(Router) {
+
+    // HTML GET Requests
+
+    router.get("/exercise", function (req, res){
+
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+
+    // if not matching route is found default to index
+
+    router.get("*", function (req, res){
+
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+
+};
